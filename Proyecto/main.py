15137925938ef9ae5086760b51a740fd8275cb11
PrisWen <priscila_wendy@hotmail.com>
2020-05-08 23:08:01 -0400

    Python OpenCV: Converting an image to black and white, resizing and tile an image

--- Proyecto/main.py
+++ Proyecto/main.py
@@ -1,5 +1,5 @@
 import cv2
-import re
+import numpy
 
 
 def read_image(path_img):
@@ -12,6 +12,12 @@ def img_to_gray_scale(img):
     """Receive an obj image, the return a gray image"""
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     return img_gray
+
+
+def img_to_black_and_white(img):
+    img_gray = img_to_gray_scale(img)
+    (thresh, black_white) = cv2.threshold(img_gray, 120, 255, cv2.THRESH_BINARY)
+    return black_white
 
 
 def save_image(img, name_img):
@@ -29,12 +35,30 @@ def image_to_flip_h(img):
     return img_flipHorizontal
 
 
+def resize_image(img, fx, fy):
+    resized = cv2.resize(img, None, fx=fx, fy=fy)
+    return resized
+
+
+def tile_image(img, repetitions_vertically, repetitions_horizontally):
+    tile = numpy.tile(img, (repetitions_vertically, repetitions_horizontally, 1));
+    return tile
+
+
 if __name__ == '__main__':
+
     img = read_image("images/conejitos.jpg")
-    gray_image = img_to_gray_scale(img)
-    save_image(gray_image, "img_gray.jpg")
-    flip_h_image = image_to_flip_h(img)
-    save_image(flip_h_image, "flip_h.jpg")
-    flip_v_image= image_to_flip_v(img)
-    save_image(flip_v_image, "flip_v.jpg")
+    # gray_image = img_to_gray_scale(img)
+    # save_image(gray_image, "img_gray.jpg")
+    # flip_h_image = image_to_flip_h(img)
+    # save_image(flip_h_image, "flip_h.jpg")
+    # flip_v_image = image_to_flip_v(img)
+    # save_image(flip_v_image, "flip_v.jpg")
+    # img_resize = resize_image(img, 2.0, 2.0)
+    # save_image(img_resize, "resize.jpg")
+    # img_tile = tile_image(img, 2, 3)
+    # save_image(img_tile, "tile.jpg")
+    img_black_white = img_to_black_and_white(img)
+    save_image(img_black_white, "black_white.jpg")
+
 
